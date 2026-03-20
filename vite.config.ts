80f4dadc1c6a3676ad496cdd6130a371618815be
Dark Whyte <safelane.tnts@gmail.com>
2026-03-19 21:03:22 -0500

    Update base path in vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,6 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "./",
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    target: "esnext",
-  },
+  base: '/TheRedRoom/',
+  // other configuration settings
 });
