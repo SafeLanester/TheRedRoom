24bdf838e7f4869bfa5a05445e0583aa7c18148e
Dark Whyte <safelane.tnts@gmail.com>
2026-04-16 16:33:08 -0400

    Add PostCSS support to Vite config
    Page wasn't displaying any type of styling. With the help of copilot, we quickly seen that tailwind and post wasn't being loaded.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import path from "node:path";
 
 export default defineConfig({
   base: '/TheRedRoom/',
+  css: {
+    postcss: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
